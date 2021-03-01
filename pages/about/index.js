dec3fdc842350b90dc793f9cd419bf6d339a7c6a
Stephen Ro <stephenkro@gmail.com>
2021-03-01 16:11:32 -0500

    basic setup for portfolio using next.js

---
+++ pages/about/index.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About</h1>
+      
+    </Layout>
+  );
+}
---
+++ pages/projects/index.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function Projects() {
+    return (
+      <Layout>
+        <Head>
+          <title>Projects</title>
+        </Head>
+        <h1>Projects</h1>
+        
+      </Layout>
+    );
+  }
